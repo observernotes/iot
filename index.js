56e65e89ad80d3aaf510e88e179f7bba4dd8c501
lilongllong <2453085960@qq.com>
2017-09-01 12:43:56 +0000

    add cmu

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var rpio = require('rpio');
 var process = require('child_process');
+var request =  require('superagent');
 rpio.open(11, rpio.INPUT);
 
 var faceLastState = 0;
@@ -10,13 +11,22 @@ while(true) {
 		console.log('video on and loading...');
 		toggleFace();
 		console.log('video loaded and uploaded.');
+		sendToServer();
 	}
 	faceLastState = faceValue;
 	rpio.msleep(200);
 }
 
 function toggleFace() {
-	process.exec('raspivid -o facevideo.h264 -t 10000', function(error, stdout, stderr) {
+	process.exec('raspivid -o facevideo.h264 -t 5000', function(error, stdout, stderr) {
 		console.log('video loaded amd uploaded.');
 	});	
 } 
+
+function sendToServer() {
+	console.log('ready to upload');
+	request.post('http://30.131.35.131:3000/faceMatch').send({ command: 'face detect', data: {}}).end(function(err, res){
+		console.log(res)
+	});
+console.log('ready to upload');
+}
