58910a09d8b4385749df119613ef0719e9be63bd
lilongllong <2453085960@qq.com>
2017-09-01 12:57:27 +0000

    add cum

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ var faceLastState = 0;
 while(true) {
 	var faceValue = rpio.read(11);
 	if (faceValue !== faceLastState && faceValue === 1) {
-		console.log('video on and loading...');
-		toggleFace();
-		console.log('video loaded and uploaded.');
-		sendToServer();
+		break;
 	}
 	faceLastState = faceValue;
 	rpio.msleep(200);
 }
+console.log('video on and loading...');
+toggleFace();
+sendToServer();
 
 function toggleFace() {
 	process.exec('raspivid -o facevideo.h264 -t 5000', function(error, stdout, stderr) {
 		console.log('video loaded amd uploaded.');
 	});	
 } 
+toggleFace();
+sendToServer();
 
 function sendToServer() {
 	console.log('ready to upload');
 	request.post('http://30.131.35.131:3000/faceMatch').send({ command: 'face detect', data: {}}).end(function(err, res){
-		console.log(res)
+		console.log('ready to upload');
 	});
-console.log('ready to upload');
 }
